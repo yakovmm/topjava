725564d78bdb32da7f0f4dcda460326100eb2ca1
yakovmm <yakovmm@mail.ru>
2020-07-12 11:22:09 +0300

    Add method isBetween to TimeUtil class

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -6,4 +6,9 @@ public class TimeUtil {
     public static boolean isBetweenInclusive(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
+
+    public static boolean isBetween(LocalTime time, LocalTime startTime, LocalTime endTime) {
+//        TODO develop this method
+        return false;
+    }
 }
